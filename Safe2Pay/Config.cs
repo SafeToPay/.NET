191f3ae441e8569fc1bc7715cb6eea2783d4cbf9
Felipe Coelho <felipe.coelho@safeweb.com.br>
2019-07-29 15:40:54 -0300

    Remoção da propriedade do timeout da Config, já tratada no RetryHandler.

--- Safe2Pay/Config.cs
+++ Safe2Pay/Config.cs
@@ -2,15 +2,13 @@
 {
     public class Config
     {
-        public Config(string token, string secretKey, int timeout = 1)
+        public Config(string token, string secretKey)
         {
-            Timeout = timeout;
             Token = token;
             SecretKey = secretKey;
         }
 
         public string Token { get; private set; }
         public string SecretKey { get; private set; }
-        public int Timeout { get; private set; }
     }
 }
