bbc968926017149f2cb4b9dc862ddb62f1c38d32
Felipe Coelho <felipe.coelho@safeweb.com.br>
2019-08-29 20:02:23 -0300

    Adicionar propriedade de resposta.

--- Safe2Pay/Models/CheckoutResponse.cs
+++ Safe2Pay/Models/CheckoutResponse.cs
@@ -30,6 +30,7 @@ namespace Safe2Pay.Models
         public string DocType { get; set; }
         public string Accept { get; set; }
         public string GuarantorName { get; set; }
+        public string GuarantorIdentity { get; set; }
         public decimal? AdditionPayment { get; set; }
         public decimal? DiscountPayment { get; set; }
 
