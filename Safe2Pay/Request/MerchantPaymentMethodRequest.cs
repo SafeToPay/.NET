e33bcac8b479c72e8da1ecac5ff899ca5b7bf278
Gabriel Vianna Garcia <gabriel.garcia@safe2pay.com.br>
2021-11-30 09:41:20 -0300

    PaymentMethods

---
+++ Safe2Pay/Request/MerchantPaymentMethodRequest.cs
@@ -0,0 +1,25 @@
+﻿using Safe2Pay.Core;
+using Safe2Pay.Response;
+using System;
+
+namespace Safe2Pay.Request
+{
+    public class MerchantPaymentMethodRequest
+    {
+        private Client Client { get; }
+
+        /// <summary>
+        /// Construtor para as funções de formas de pagamento.
+        /// </summary>
+        /// <param name="config">Dados de autenticação na API.</param>
+        public MerchantPaymentMethodRequest(Config config) => Client = new Client(config);
+
+        /// <summary>
+        /// Consultar formas de pagamento da empresa
+        /// </summary>
+        public MerchantPaymentMethodResponse List()
+        {
+            return Client.Get<MerchantPaymentMethodResponse>(false, "v2/MerchantPaymentMethod/List").GetAwaiter().GetResult();
+        }
+    }
+}
---
+++ Safe2Pay/Response/MerchantPaymentMethodResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Safe2Pay.Response
+{
+    public class MerchantPaymentMethodResponse
+    {
+        public PaymentMethodResponse PaymentMethod { get; set; }
+        public bool IsEnabled { get; set; }
+        public decimal InstallmentLimit { get; set; }
+        public decimal MinorInstallmentAmount { get; set; }
+        public decimal IsInstallmentEnable { get; set; }
+    }
+}
